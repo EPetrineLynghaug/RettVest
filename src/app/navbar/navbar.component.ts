ce4e430f673f6549eaa4f9669fcf74a33cdb1c9c
EPetrineLynghaug <136695793+EPetrineLynghaug@users.noreply.github.com>
2023-12-21 20:22:10 +0100

    mobile first- nav bar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterModule,
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
+  menuOpen: boolean = false;
+
+  // menuItems: Array<MenuItem> = [];
+  menuItems: MenuItem[] = [
+    {
+       id: 0,
+       text: "Hjemmekennel & pensjonat",
+       href: "/hjemmekennel",
+    },
+    {
+       id: 1,
+       text: "Om oss",
+       href: "/omoss",
+    },
+  {
+    id: 2,
+    text: "RAWFÔR",
+    href: "/rawför",
+ },
+ {
+  id: 3,
+  text: "Kontakt oss",
+  href: "/kontakt",
+},
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+  }
+
+}
+
+interface MenuItem {
+  id: Number;
+  text: String;
+  href: String;
+  icon?: String; // Optional
 }
