0269bd39e8f720221d19ff6937cd2713a6a27b0c
EPetrineLynghaug <136695793+EPetrineLynghaug@users.noreply.github.com>
2024-02-04 00:13:26 +0100

    form test.

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,31 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [
-    FormsModule,
-  ],
+  imports: [FormsModule],
   templateUrl: './contact-form.component.html',
-  styleUrl: './contact-form.component.scss'
+  styleUrl: './contact-form.component.scss',
 })
-export class ContactFormComponent implements OnInit {
-
+export class ContactFormComponent implements AfterViewInit {
   messageLimit: number = 250;
   messageLength: number = 0;
   disabled: boolean = false;
+  formSubmitted: boolean = false;
 
   constructor() {}
 
-  ngOnInit(): void { }
+  ngAfterViewInit(): void {
+    let form: HTMLFormElement = document.querySelector('.contact-form') as HTMLFormElement;
+    form.addEventListener('submit', this.handleSubmit);
+  }
 
   updateMessage(length: number): void {
     this.messageLength = length;
-    
+
     this.messageLength >= this.messageLimit
-      ? this.disabled = true
-      : this.disabled = false;
+      ? (this.disabled = true)
+      : (this.disabled = false);
   }
 
+  handleSubmit(event: any): void {
+    event.preventDefault();
+
+    const myForm: any = event.target;
+    const formData: any = new FormData(myForm);
+
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => {
+        console.log('Form successfully submitted');
+        this.formSubmitted = true;
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  };
 }
